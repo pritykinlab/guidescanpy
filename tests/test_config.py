96b11c68c94d470693c0f39f7269be1b4988af44
Vineet Bansal <vineetbansal@protonmail.com>
2023-07-11 14:04:36 -0400

    envvar support for config

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,15 +2,19 @@ from guidescanpy import config
 
 
 def test_config_string():
-    assert isinstance(config.guidescan.bin, str)
+    assert config.pytest.astring == "foo"
 
 
 def test_config_string_bool():
-    assert config.celery.eager in (True, False)
+    assert config.pytest.abool is True
+
+
+def test_config_nested_int():
+    assert config.pytest.nested.aint == 42
 
 
 def test_config_string_override():
-    old_value = config.guidescan.bin
-    with config({"guidescan.bin": "foo"}):
-        assert config.guidescan.bin == "foo"
-    assert config.guidescan.bin == old_value
+    old_value = config.pytest.astring
+    with config({"pytest.astring": "bar"}):
+        assert config.pytest.astring == "bar"
+    assert config.pytest.astring == old_value
